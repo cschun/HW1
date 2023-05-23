4e76c762f3fe2ae5954e0e982cfb2368aa5775c4
Clara Chun <clarachun@aol.com>
2023-05-23 00:16:27 -0700

    latest update

--- idk.py
+++ idk.py
@@ -4,7 +4,7 @@ from matplotlib.animation import FuncAnimation
 
 
 t = 0  # current time of the simulation
-tEnd = 1.5   # time at which simulation ends
+tEnd = .5   # time at which simulation ends
 dt = 0.001  # timestep
 #rmin in kpc
 rmin = 25
@@ -13,7 +13,7 @@ m1 = 10e11
 m2 = 10e11
 #G in terms of solar masses and kpc 
 G = 4.300917270038e-06
-e = 0.6
+e = 0.5
 
 #Functions to get initial values in r,phi
 def r(phi):
@@ -93,8 +93,7 @@ def getAcc(pos, mass, G, softening):
 
     return a
 
-def leapfrog(acc, vel, mass, pos, Nt, t):
-    pos_save = np.zeros((N, 3, Nt + 1))
+def leapfrog(acc, vel, pos, Nt, t):
     for i in range(Nt):
         # (1/2) kick
         vel += acc * dt / 2.0
@@ -111,6 +110,7 @@ def leapfrog(acc, vel, mass, pos, Nt, t):
         # update time
         t += dt
 
+        # save energies, positions for plotting trail
         pos_save[:, :, i + 1] = pos
     return pos_save
 
@@ -121,21 +121,21 @@ def initial_conditions(M):
     
     for i in range(12):
         j = 12+(3*i)
-        rad = (0.2+(i*0.05))*rmin
+        rad = (0.2+(i*0.5))*rmin
         angles = np.linspace(0,1,j)*(np.pi*2)
 
         pos.extend(np.stack((rad*np.cos(angles), rad*np.sin(angles), np.zeros(len(angles))), axis = 1))
         r.extend(np.full(shape=j, fill_value=rad))
         
         v0 = np.sqrt(G*M*rad/(rad**2+softening**2))
-        vel.extend(np.stack((v0*np.sin(angles), v0*np.cos(angles), np.zeros(len(angles))), axis = 1))
+        vel.extend(np.stack((v0*np.cos(angles), v0*np.sin(angles), np.zeros(len(angles))), axis = 1))
 
 
     r = np.array(r, float)
 
     return pos, vel, r
 
-def animation_graph(xx, yy, zz):
+def animation_graph(xx, yy):
     fig = plt.figure()
     ax = plt.axes(xlim=(-40, 40), ylim=(-30, 30), xlabel="x", ylabel="v")
     line, = ax.plot([], [], lw=2, marker=".")
@@ -161,23 +161,10 @@ def animation_graph(xx, yy, zz):
 
     plt.close()
 
-def static_graph(xx, yy, zz):
-    fig = plt.figure()
-    ax = plt.axes(projection='3d')
-    ax.scatter3D(xx[1], yy[1], zz[1],  s=5)
-    ax.scatter3D(xx[2], yy[2], zz[2],  s=5)
-    ax.scatter3D(xx[3], yy[3], zz[2],  s=5)
-    ax.scatter3D(xx[0], yy[0], zz[0],  s=5)
+def static_graph(xx, yy):
+    plt.scatter(xx[1], yy[1],  s=5)
+    plt.scatter(xx[2], yy[2],  s=5)
     plt.savefig("2body_orbit.png")
-
-def static_graph2(xx, yy, zz):
-    fig = plt.figure()
-    ax = plt.axes()
-    ax.scatter(xx[0], yy[0],  s=5)
-    ax.scatter(xx[1], yy[1],  s=5)
-    ax.scatter(xx[2], yy[2],  s=5)
-    ax.scatter(xx[3], yy[3],  s=5)
-    plt.savefig("2body_orbit2.png")
 
 def rotation_matrix(i):
     a = 1-np.cos(-i)
@@ -186,34 +173,10 @@ def rotation_matrix(i):
     x = np.cos(-90)
     y = np.sin(-90)
     z = 0
-    rotation = [[(a*(x**2))+c, a*x*y, s*y],
-                [a*x*y, (a*(y**2))+c, -s*x],
-                [-s*y, s*x, (a*(z**2))+c]]
+    rotation = [[a*x**2+c, a*x*y-s*z, a*x*z+s*y],
+                [a*x*y+s*z, a*y**2+c, a*z*y-s*x],
+                [a*x*z*-s*y, a*y*z+s*x, a*z**2+c]]
     return rotation
-
-def graph(x, y, z, t):
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    label = ['galaxy 1, galaxy 2']
-    color = ['magenta','blue']
-    for i in range(len(t_all)):
-        for i in range(1):
-            line, = ax.plot(x[i], y[i], z[i], label=label[i], c=color[i])
-
-    point_gal, = ax.plot(init_index['x'][1], init_index['y'][1],init_index['z'][1], 'o', color='darkblue')
-    point_int, = ax.plot(init_index['x'][0], init_index['y'][0],init_index['z'][0], 'o', color='darkviolet')
-    point, = ax.plot(init_index['x'][2:], init_index['y'][2:], init_index['z'][2:],'o',color = 'cornflowerblue', markersize=1)
-
-
-    ax.legend()
-    ax.set_xlim([-500, 500])
-    ax.set_ylim([-500, 500])
-    ax.set_zlim([-500, 500])
-    anim = FuncAnimation(fig, update_point, 1000, fargs=(x, y, z))
-    anim.save('final_testing.mp4', fps=60, dpi=200, extra_args=['-vcodec','libx264'])
-    plt.savefig('cartwheel_galaxy_simulation.png', dpi=200)
-    plt.show()
-
 
 if __name__ == "__main__":
     N = 3
@@ -221,56 +184,38 @@ if __name__ == "__main__":
 
     m1pos, m1vel, m1r = initial_conditions(m1)
     m2pos, m2vel, m2r = initial_conditions(m2)
- 
-    m1pos = np.array(m1pos)@rotation_matrix(15) 
-    m2pos = np.array(m2pos)@rotation_matrix(60) 
-    m1vel = np.array(m1vel)@rotation_matrix(15) 
-    m2vel = np.array(m2vel)@rotation_matrix(60)
 
     m1pos = [x - [x1(0), y1(0), 0] for x in m1pos]
-    m2pos = [x - [x2(0), y2(0), 0] for x in m2pos]
+    m2pos = [x - [x2(0), y2(0), 0] for x in m1pos]
+
     m1vel = [x + [vx1(0), vy1(0), 0] for x in m1vel]
     m2vel = [x + [vx2(0), vy2(0), 0] for x in m2vel]
-     
-    
+
     #setting up conditions
     Nt = int(np.ceil(tEnd / dt))
 
-    pos_array = np.zeros((len(m1pos)*2, 3, Nt+1))
+    pos_array = np.zeros((len(m1pos)+2, 3, Nt+1))
 
     for i in range(2):
-        mass1 = np.array([[m1], [0], [0]])
-        pos1 = np.array([[x1(0), y1(0), 0], [x2(0), y2(0), 0], m1pos[i]])
-        vel1 = np.array([[vx1(0), vy1(0), 0], [vx2(0), vy2(0), 0], m1vel[i]])
+        mass = np.array([[m1], [m2], [0]])
+        pos = np.array([[x1(0), y1(0), 0], [x2(0), y2(0), 0], m1pos[i]])
+        vel = np.array([[vx1(0), vy1(0), 0], [vx2(0), vy2(0), 0], m1vel[i]])
 
-        mass2 = np.array([[m1], [m2], [0]])
-        pos2 = np.array([[x1(0), y1(0), 0], [x2(0), y2(0), 0], m2pos[i]])
-        vel2 = np.array([[vx1(0), vy1(0), 0], [vx2(0), vy2(0), 0], m2vel[i]])
+        vel -= np.mean(mass * vel, 0) / np.mean(mass)
 
-        vel1 -= np.mean(mass1 * vel1, 0) / np.mean(mass1)
-        vel2 -= np.mean(mass2 * vel2, 0) / np.mean(mass2)
+        acc = getAcc(pos, mass, G, softening)
 
-        acc1 = getAcc(pos1, mass1, G, softening)
-        acc2 = getAcc(pos2, mass2, G, softening)
+        pos_save = np.zeros((N, 3, Nt + 1))
+        pos_save[:, :, 0] = pos
+        t_all = np.arange(Nt + 1) * dt
 
-        pos_save1 = np.zeros((N, 3, Nt + 1))
-        pos_save1[:, :, 0] = pos1
-
-        pos_save2 = np.zeros((N, 3, Nt + 1))
-        pos_save2[:, :, 0] = pos2
-
-        pos_save1 = leapfrog(acc1, vel1, mass1, pos1, Nt, t)
-        pos_save2 = leapfrog(acc2, vel2, mass2, pos2, Nt, t)
-
-        pos_array[0] = pos_save1[0]
-        pos_array[1] = pos_save1[1]
-        pos_array[i+2] = pos_save1[2]
-        pos_array[(i+2)*2] = pos_save2[2]
+        pos_save = leapfrog(acc, vel, pos, Nt, t)
+        pos_array[0] = pos_save[0]
+        pos_array[1] = pos_save[1]
+        pos_array[i+2] = pos_save[2]
 
     xx = pos_array[:, 0, :]
     yy = pos_array[:, 1, :]
-    zz = pos_array[:, 2, :]
-    t_all = np.arange(Nt + 1) * dt
 
-    #animation_graph(xx, yy, zz)
-    graph(xx, yy, zz, t_all)
+    #animation_graph(xx, yy)
+    static_graph(xx, yy)
